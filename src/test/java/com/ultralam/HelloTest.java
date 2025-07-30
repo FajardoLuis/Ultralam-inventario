d1a826e6e94c6bfb4ef2609c449e803b0b41cd8f
al03047899@tecmilenio.com <al03047899@tecmilenio.mx>
2025-07-29 21:13:50 -0600

    Agregar prueba JUnit basica

---
+++ src/test/java/com/ultralam/HelloTest.java
@@ -0,0 +1,12 @@
+package com.ultralam;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloTest {
+    
+    @Test
+    public void testTrueIsTrue() {
+        assertTrue(true);
+    }
+}
